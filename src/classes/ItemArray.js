dc8f29844d1ac91046353fe6206b055deb4ca8ff
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-05-04 14:16:18 +0200

    Update ItemArray.js

--- src/classes/ItemArray.js
+++ src/classes/ItemArray.js
@@ -68,7 +68,7 @@ export let ItemArray = [
     "Tommy Shelby, a dangerous man, leads the Peaky Blinders, a gang based in Birmingham. Soon, Chester Campbell, an inspector, decides to nab him and put an end to the criminal activities.",
     "2013",
     false,
-    "/src/images/peaky-blinders.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABUd4OjUOm6l3ZQsBwVcCSJCbuOdHaa3l62lvie0HktgkfNQzSne_JXXEfqIexR_BNAUnpFoYQFfsMaoQHR7MTzXZPBM.webp?r=b23",
     "https://www.youtube.com/watch?v=jImOqxQ0kkM"
   ),
   new Item(
@@ -80,7 +80,7 @@ export let ItemArray = [
     "After a zombie outbreak in Las Vegas, a group of mercenaries take the ultimate gamble and venture into the quarantine zone in hopes of pulling off an impossible heist.",
     "2013",
     false,
-    "/src/images/army-of-the-dead.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABVcK1i7ZKx45sYmAkH6IhB4XgiBFgh_On-Vq9ab2jofY4ZTqFd_k2xS4o1nN9U-trafoaJG9Y_UdhbWgovTXKeAEpUkY3El0c-gPlanhk4zjuKX1W4-beyBUz4zQ.jpg?r=f60",
     "https://www.youtube.com/watch?v=47nS6Wo4yaw"
   ),
   new Item(
@@ -92,7 +92,7 @@ export let ItemArray = [
     "This collection of animated short stories spans several genres, including science fiction, fantasy, horror and comedy. World-class animation creators bring captivating stories to life in the form of a unique and visceral viewing experience.",
     "2019",
     false,
-    "/src/images/love-death-robots.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQvxXNAtHZO0L3elCihVEFr9kUl58LC4hz89kuhtxCpLtRlo099P722G-xO_L3vCpPV9zJt8UwjaLkd0n-j5ZFrWucMP0a-YMLYvO9uO14slVUVj3nZa4t51E2Av.jpg?r=4a1",
     "https://www.youtube.com/watch?v=wUFwunMKa4E"
   ),
   new Item(
@@ -104,7 +104,7 @@ export let ItemArray = [
     "With the help of technology, Callum learns that he is a descendant of a dangerous society. He travels back in time to learn about his ancestors and acquire skills to defeat an evil organisation.",
     "2016",
     false,
-    "/src/images/assassins-creed.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABbsZK9iiyMx24PrVuAIZjMLtnPuntvjXn25jAecGjcAHrr5VCMzWj5-5CFFRlTeDHpguwn3s_-RILvcA_CX44er9Dss.webp?r=86f",
     "https://www.youtube.com/watch?v=gfJVoF5ko1Y"
   ),
   new Item(
@@ -116,7 +116,7 @@ export let ItemArray = [
     "Ragnar Lothbrok, a legendary Norse hero, is a mere farmer who rises up to become a fearless warrior and commander of the Viking tribes with the support of his equally ferocious family.",
     "2013",
     false,
-    "/src/images/vikings.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABew4t0MGVYUc9NlgkhkkGdOneHGnSP-3sFNcKSirxgYD3e0ztGhEOuf53bKBjP6gslsKz_vWBVwSF2rgLObI4dwWnpE.webp?r=b2a",
     "https://www.youtube.com/watch?v=9GgxinPwAGc"
   ),
   new Item(
@@ -128,7 +128,7 @@ export let ItemArray = [
     "When Django, a slave, is freed, he joins forces with a bounty hunter to rescue his wife, who has been enslaved by Calvin, a hard-hearted plantation owner.",
     "2012",
     false,
-    "/src/images/django-unchained.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABYDfH3Mb6FJT8aqSLf7tndN2GsqZR2kyLF1PZPLXOp7erQRzGbaaBAaxpnjjWZbfD-YtLC8eiWWhxmFx2AzEUshEX3c.webp?r=cfc",
     "https://www.youtube.com/watch?v=_iH0UBYDI4g"
   ),
 
@@ -142,7 +142,7 @@ export let ItemArray = [
     "A motley group of office workers go through hilarious misadventures at the Scranton, Pennsylvania, branch of the Dunder Mifflin Paper Company.",
     "2005",
     false,
-    "/src/images/the-office.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABYLt72qjtRHlzDyev_3QQLCFQ2QT7ZZoGBAtsQFJ2kvOGpy2q6b40qDr4May_YGdSpHWctpBb3EKfazSCI8GJc2GYVI.webp?r=2c8",
     "https://www.youtube.com/watch?v=2iKZmRR9AR4"
   ),
   new Item(
@@ -154,7 +154,7 @@ export let ItemArray = [
     "Shrek, an ogre, embarks on a journey with a donkey to rescue Princess Fiona from a vile lord and regain his swamp.",
     "2001",
     false,
-    "/src/images/shrek.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABUIOAE8m-uMiqaPW-dJxk0oWNhjd2a8BD5gPHjLm-3r5CPNBQZc0OnbL_ZgtHREgBD-Fx15DfquYK12RJVxhr02XwLw.webp?r=c17",
     "https://www.youtube.com/watch?v=CwXOrWvPBPk"
   ),
   new Item(
@@ -166,7 +166,7 @@ export let ItemArray = [
     "Follow the Murphy family back to the 1970s, when kids roamed wild, beer flowed freely and nothing came between a man and his TV.",
     "2015",
     false,
-    "/src/images/f-is-for-family.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABayk3DEH0KayU3fGm0ncsfGYWEZjzSmdqA3I1pAevGny01LcXjC3hbA4YfVdOlCK0wZcCZ9ddyQqYbOeOhrwkkPw_ypJF_y1U_l4JJy1CYJzWnzLK2zJZ0_s18PM.jpg?r=a51",
     "https://www.youtube.com/watch?v=f2AmK5QVtbg"
   ),
   new Item(
@@ -178,7 +178,7 @@ export let ItemArray = [
     "Rod, a naif, slacker lives with his mother, brother and stepfather whose respect he craves. When his stepfather needs an operation, to raise funds, he plans to set a school-bus-jumping record.",
     "2007",
     false,
-    "/src/images/hot-rod.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABZykgY3hvcIOW7VUKeZi061xw10Wm9AFYEIDdFgktLsEMwhBvWwFNSpeWyIZDxUWY_j6-1nvddNSCKWGkuNKWuVg7Ww.webp?r=9ab",
     "https://www.youtube.com/watch?v=yByhd7FAOug"
   ),
   new Item(
@@ -190,7 +190,7 @@ export let ItemArray = [
     "A group of people are tasked with establishing the U.S. Space Force.",
     "2020",
     false,
-    "/src/images/space-force.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABeS0NsjUfL-GZdLTM4dZCV2YOejpTMQhfDXPXcS3coLbn0pKLhAoM9YMjKQqp2D0lgriGAppqmxVT1YAArsCFLGsem28OAK_-hXyAVaslLflx1SAtwbSZ8auR9tP.jpg?r=fe2",
     "https://www.youtube.com/watch?v=bdpYpulGCKc"
   ),
   new Item(
@@ -202,7 +202,7 @@ export let ItemArray = [
     "An aspiring gangster pretends to be part of a notorious gang and tries to terrorise a neighbourhood. However, unknown to him, three martial arts experts live there and are ready to take him down.",
     "2004",
     false,
-    "/src/images/kung-fu-hustle.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABW07znZQr0FWx3XhxMQVYqKSgdK8-6VSMU9SuWUzHMgHXaVbFNNvM4LxVaMjHf94FYRKz3DTSZoxrK494w1UAtMeOmk.webp?r=54c",
     "https://www.youtube.com/watch?v=47nS6Wo4yaw"
   ),
 
@@ -216,7 +216,7 @@ export let ItemArray = [
     "Lorelai Gilmore, an independent young woman, shares a close bond with her wilful daughter, Rory. However, her relationship with her own mother is strained.",
     "2000",
     false,
-    "/src/images/gilmore-girls.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQjI7bNcrihPct-wVORDkBW3uHtx0yM5Clhc1E_98-WycEovklhLJu8VtYWVrePRtMvi1ama2tLWQnP5N-AVi4Bl0to.webp?r=037",
     "https://www.youtube.com/watch?v=HvbhPt0z4yE"
   ),
   new Item(
@@ -228,7 +228,7 @@ export let ItemArray = [
     "After Mirta dies of a drug overdose with her lover, she resuscitates alone and discovers she's part of a violent world she never knew existed.",
     "2021",
     false,
-    "/src/images/dont-kill-me.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABTWh2tgSzcb3axDGqtsfAGDPUIAZFNhmB3v7d_qFM3yKrM37MM5bSyWOg7FPOhzQqF-lE0m8D4QJ6xVyUI2D1pe7YbklhLjBwXMAty0mnFFhJoV6666AfrPLoMeH.jpg?r=944",
     "https://www.youtube.com/watch?v=2iKZmRR9AR4"
   ),
   new Item(
@@ -240,7 +240,7 @@ export let ItemArray = [
     "In a rough Los Angeles neighborhood, four smart, funny and streetwise teens find their lifelong friendship tested as they begin high school. Watch Chapter One.",
     "2018",
     false,
-    "/src/images/on-my-block.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQubm1X71oW8fQyzYWEKfL1JDEkraZ_RqOIKz1YtS39KQRK0knVbGSikc0VyRY2bEDiIEdxaEZwinlkVTXq-F8OpFUCqjh5nUspxCiWvYsgVrj8tUEVakaDoDDn5.jpg?r=f74",
     "https://www.youtube.com/watch?v=WlZvfarViqc"
   ),
   new Item(
@@ -252,7 +252,7 @@ export let ItemArray = [
     "Secretly in love with her classmate Kento, Miyo takes the help of a mysterious mask and transforms into a cat to get closer to him. However, trouble ensues when she begins to lose herself.",
     "2020",
     false,
-    "/src/images/a-whisker-away.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABetcWkGikSLjMEHEXXu90Czs_pSiBwgTRzO_MTT_lktHiTOgZLE70hAEoTO1nd-dZ0kyZ778CaXWEyAAiL6OsfJM1_QVmUTxHJsTvInjG2MR9OHTGqAtMPWraS8g.jpg?r=192",
     "https://www.youtube.com/watch?v=aXc9DVfLTGo"
   ),
   new Item(
@@ -264,7 +264,7 @@ export let ItemArray = [
     "In 17th-century Italy, a teenage midwife accused of witchcraft must choose between fulfilling her powerful destiny and a star-crossed love.",
     "2020",
     false,
-    "/src/images/luna-nera.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABbDCLVi3fLqRRc-HTnsOJq-JODUbedYiiQUYeeuLi7dB8TJlOG9MG4pP42W3N4OXS3vAmoB-fbq8Ykk1UbNyh9uWfM7kbp9kBp_Si9FxONjccbXkeZSFVXHH11oy.jpg?r=7d9",
     "https://www.youtube.com/watch?v=H4oLu4cP7h4"
   ),
   new Item(
@@ -276,7 +276,7 @@ export let ItemArray = [
     "Forrest, a man with low IQ, recounts the early years of his life when he found himself in the middle of key historical events. All he wants now is to be reunited with his childhood sweetheart, Jenny.",
     "1994",
     false,
-    "/src/images/forrest-gump.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQGyOBl1aa_RQ3Av5tJiYErMEWzW71L0PqcXlXNBMz4xA5sCGi7s5e1_dOpjLbv_a3PbdF-H-jjzLDsfn5Ed742W10s.webp?r=e7f",
     "https://www.youtube.com/watch?v=Hld-7oBn3Rk"
   ),
 
@@ -290,7 +290,7 @@ export let ItemArray = [
     "In 1980s Indiana, a group of young friends witness supernatural forces and secret government exploits. As they search for answers, the children unravel a series of extraordinary mysteries.",
     "2016",
     false,
-    "/src/images/stranger-things.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABczAB1eXgyGj5cZ8Kq_qKb2tA34F2X_5pIaNXCYbeIZq5gKWZ-7J06NbIRyinoX0idvVGiHoL-bakkadoweu6o9LB-7oTZ00wYHeWW-FgCN4T7Ih3DUPED6dfyGZ.jpg?r=02b",
     "https://www.youtube.com/watch?v=mnd7sFt5c3A"
   ),
   new Item(
@@ -302,7 +302,7 @@ export let ItemArray = [
     "Genetically modified anacondas and giant crocodiles escape captivity and enter a nearby lake. When the two massive creatures fight, the sheriff tries to prevent the town from being destroyed.",
     "2015",
     false,
-    "/src/images/lp-vs-a.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABfgMo2hJEZAjRuYXac0E6fc3XnH6QbwPHq2YjRSyIuHZlbfmE-m-IGKPAcBZLWVcK_pq5qYz6nRr7b5bICm2NzVjJNI.webp?r=122",
     "https://www.youtube.com/watch?v=u_AIV9c0FWc"
   ),
   new Item(
@@ -314,7 +314,7 @@ export let ItemArray = [
     "In the wake of a zombie apocalypse, various survivors struggle to stay alive. As they search for safety and evade the undead, they are forced to grapple with rival groups and difficult choices.",
     "2010",
     false,
-    "/src/images/walking-dead.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABRU90Xa7jp2Sm7QWbw34LEhVxq0JwCOut7Zuxg91HDzbmPZeuOYm_7cWz-TBVhRKDcvUYzgiqcKCLmQjdup2FEJILFM.webp?r=cea",
     "https://www.youtube.com/watch?v=R1v0uFms68U"
   ),
   new Item(
@@ -326,7 +326,7 @@ export let ItemArray = [
     "Influencers looking to breathe new life into a Texas ghost town encounter Leatherface, the legendary killer who wears a mask of human skin.",
     "2022",
     false,
-    "/src/images/texas-chainsaw-massacre.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABWRW2cDmpfW8s6S9jDdxcTbcX9tFHDEsHZJ9VsF9HYUbCIqUjmY7kZVMad8gObaoXccUdJg41lF4y2jvjcPQWuVNVvjyLzoAqd4Mit6PpqkrhiD9Q5zSudkJDWPG.jpg?r=597",
     "https://www.youtube.com/watch?v=zcI6SFiK_yk"
   ),
   new Item(
@@ -338,7 +338,7 @@ export let ItemArray = [
     "Thirty years later, Ash Williams demon hunter par excellence is back to battle a new Deadite plague. He may have lost a hand, but not his touch.",
     "2015",
     false,
-    "/src/images/ash-vs-evil-dead.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABXDDr0dXBDa1RRs7gZsnSXuZATr95Pfg7WLwf7N2aFUQkr_-TAhyP-vUD6jiXcR7CmSd3ERZSmQ8uB8TSyx7AwIQz64.webp?r=c07",
     "https://www.youtube.com/watch?v=-Yq3jeOmxSk"
   ),
   new Item(
@@ -350,7 +350,7 @@ export let ItemArray = [
     "A deaf writer who retreated into the woods to live a solitary life must fight for her life in silence when a masked killer appears in her window.",
     "2016",
     false,
-    "/src/images/hush.jpg",
+    "https://occ-0-4305-34.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQAN-wTny5igHto-n411EfN6VPo-h4iNOR_1FWsM8o7mBf5g_m7wPMznCUSQ7SISG6HLxzc_R8F6YmdJhvGJt5YpihE.webp?r=894",
     "https://www.youtube.com/watch?v=H4oLu4cP7h4"
   ),
 ];
